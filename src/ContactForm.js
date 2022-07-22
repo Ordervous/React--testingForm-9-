febe1374fc2c18c68e73e849b467caf21a302624
Brett <bdmdiving@gmail.com>
2022-07-22 19:40:13 +1000

    most of tests complete, missing form render if props undefined

--- src/ContactForm.js
+++ src/ContactForm.js
@@ -5,4 +5,34 @@
 //     It does not throw an error if the onContactAdded prop is undefined.
 
 import React from "react";
-import { Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
+
+function OnContactAdded() {
+    alert('Contact Added!')
+}
+
+function ContactForm(props) {
+
+
+    return (
+        <Form role="form">
+            <Form.Group className="mb-3" data-testId="formBasicName">
+                <Form.Label>Name</Form.Label>
+                <Form.Control type="text" data-testId="inputname" placeholder="Enter name" />
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter Email" />
+            </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPhone">
+                <Form.Label>Phone Number</Form.Label>
+                <Form.Control type="number" placeholder="Enter Phone" />
+            </Form.Group>
+            <Button variant="primary" type="submit" onClick={OnContactAdded}>Add Contact</Button>
+        </Form>
+    )
+}
+export default ContactForm
+
+// snapshot test
--- src/__tests__/ContactForm.test.js
+++ src/__tests__/ContactForm.test.js
@@ -8,17 +8,73 @@
 //     If the markup of the page changes, a snapshot test will fail.
 
 import React from "react";
-import { Form } from "react-bootstrap";
-import ContactForm from './ContactForm';
+import ContactForm from '../ContactForm';
 import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
 
 it('renders without error', () => {
     render(<ContactForm />)
 })
 
-
-
 afterEach(() => {
     jest.restoreAllMocks()
 })
+
+it('checks for a button on the page', () => {
+    render(<ContactForm />)
+    expect(screen.getByRole('button', {name: "Add Contact"})).toBeInTheDocument()
+})
+
+it('checks for a form on the page', () => {
+    render(<ContactForm />)
+    expect(screen.getByRole('form')).toBeInTheDocument()
+})
+
+it('displays the alert when the Add Contact button is clicked', () => {
+    window.alert = jest.fn()
+    render(<ContactForm />)
+    const button = screen.getByRole('button', { name: "Add Contact" })
+    fireEvent.click(button)
+    expect(window.alert).toHaveBeenCalledTimes(1)
+}) 
+
+// Testing Input fields
+it('test input Name field', () => {
+    render(<ContactForm />)
+    const inputname = screen.getByTestId("formBasicName")
+    expect(inputname).toBeInTheDocument()
+})
+
+it('pass in text to Name input field', () => {
+    render(<ContactForm />)
+    const inputname = screen.getByTestId("inputname")
+    userEvent.type(inputname, "Joe")
+
+    expect(screen.getByTestId("inputname")).toHaveValue("Joe");
+    expect(screen.queryByTestId("error-msg")).not.toBeInTheDocument();
+})
+
+it('pass in text to Name input field2', () => {
+    render(<ContactForm />)
+    const input = screen.getByTestId("inputname")
+    fireEvent.change(input, { target: { value: 'John' } })
+    expect(input.value).toBe('John')
+})
+
+it('if OnContactAdded props is undefined', () => {
+    window.alert = jest.fn()
+    render(<ContactForm />)
+    const unprops = screen.queryByText('undefined')
+    expect(unprops).not.toBeInTheDocument()
+})
+ 
+
+it('should match the snapshot', () => {
+    const spy = jest.spyOn(Date, 'now')
+      .mockImplementation(() => new Date('2021-01-01T00:00')
+    )
+    const { asFragment } = render(<ContactForm />)
+    const html = asFragment()
+    expect(html).toMatchSnapshot()
+  })
